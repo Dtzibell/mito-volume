20b7b95eefad591d624e06dfd8bf72d520490189
Dtzibell <zaleniakas.tauras@gmail.com>
2026-02-26 21:04:57 +0100

    ideas

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import numpy as np
-from File_Importer import File_Import
+# from File_Importer import File_Import
+from pathlib import Path
 
-
-z, file_name1 = File_Import()
-df1 = pd.read_csv(z)
-
-m, file_name2 = File_Import()
-df2 = pd.read_csv(m)
+ph3 = Path(r"/home/dtzi/Desktop/Position_0/Images/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_acdc_output.csv")
+mito = Path(r"/home/dtzi/Desktop/Position_0/Images/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_run_num1_mCardinal_ref_ch_acdc_output_mask_mitoacdc_outputentation.csv")
+df1 = pd.read_csv(ph3)
+df2 = pd.read_csv(mito)
+# z, file_name1 = File_Import()
+# df1 = pd.read_csv(z)
+# 
+# m, file_name2 = File_Import()
+# df2 = pd.read_csv(m)
 
 merged_df = pd.merge(df1, df2, on=['Cell_ID', 'frame_i'])
-
 
 s_df = merged_df[merged_df['cell_cycle_stage'] == 'S'].copy()
 
@@ -41,6 +44,7 @@ buds = buds.rename(columns={
     'Cell_ID': 'bud_Cell_ID',
     'mito_to_cell_ratio': 'mito_to_cell_ratio_bud'
 })
+buds.to_excel(Path("output2.xlsx"), index=False)
 
 
 mothers = mothers[
@@ -50,6 +54,7 @@ mothers = mothers[
         'mito_to_cell_ratio_mother'
     ]
 ]
+mothers.to_excel(Path("output1.xlsx"), index=False)
 
 
 mother_bud_table = buds.merge(
@@ -77,11 +82,12 @@ mother_bud_table = mother_bud_table[
     ]
 ]
 
-output_file_path = (
-    r'C:\Users\kasia\OneDrive\Desktop\Data\Data\Codes\Output from codes'
-    r'\bud_mother_endS_table.xlsx'
-)
-
-mother_bud_table.to_excel(output_file_path, index=False)
+# output_file_path = (
+#         r"output.xlsx"
+#     # r'C:\Users\kasia\OneDrive\Desktop\Data\Data\Codes\Output from codes'
+#     # r'\bud_mother_endS_table.xlsx'
+# )
+#
+# mother_bud_table.to_excel(output_file_path, index=False)
 
 print("End-of-S analysis complete.")
---
+++ with_polars/with_polars.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+import polars as pl
+from polars import col as c
+import numpy as np
+
+if __name__ == "__main__":
+    pass
