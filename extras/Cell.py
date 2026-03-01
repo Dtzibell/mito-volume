24fc3f1b53cc158a612e545fb3167cf4bfb4da02
Dtzibell <zaleniakas.tauras@gmail.com>
2026-03-01 03:28:53 +0100

    tree-like recursive implementation

--- extras/Cell.py
+++ extras/Cell.py
@@ -1,52 +1,95 @@
+from __future__ import annotations
 import polars as pl
 from polars import col as c
-from pathlib import Path
-
-mitocsv = Path(r"/home/tauras/Desktop/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_run_num1_mCardinal_ref_ch_acdc_output_mask_mitoacdc_outputentation.csv")
-ph3csv = Path(r"/home/tauras/Desktop/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_acdc_output.csv")
 
 class Cell:
-    def __init__(self, ph3):
-        self.ph3 = ph3
-        id = self.getID()
-        frame = self.getFrameID()
-        self.mito = (pl.scan_csv(mitocsv)
-                     .filter((c("Cell_ID") == id) &
-                             (c("frame_i") == frame))
-                     .collect())
-        self.parent_ph3 = (pl.scan_csv(ph3csv)
-                       .filter((c("Cell_ID") == self.getParentID()) &
-                               (c("frame_i") == self.getFrameID()))
-                       .collect())
-        self.parent_mito = (pl.scan_csv(mitocsv)
-                       .filter((c("Cell_ID") == self.getParentID()) &
-                               (c("frame_i") == self.getFrameID()))
-                       .collect())
 
-    def getMito(self):
-        pass
+    def __init__(self, id: int, parent: Cell | bool,
+                 ph3: pl.LazyFrame, mito: pl.LazyFrame):
+        self.id = id
+        self.ph3 = (ph3
+                    .filter(c("Cell_ID") == id)
+                    .collect()
+                    )
+        self.mito = (mito
+                    .filter(c("Cell_ID") == id)
+                    .collect()
+                     )
+        self.budding = self.ph3.filter(c("cell_cycle_stage") == "S")
+
+        # sets valid flag for cells that have finished a full S phase.
+        if self.budding.height < self.ph3.height:
+            self.valid = True
+        else: self.valid = False
+        if self.valid and parent:
+            self.ph3_bud_end = (self.budding
+                                     .filter(c("relationship") == "bud")
+                                     .reverse()
+                                     .unique(c("Cell_ID"))
+                                     )
+            self.mito_bud_end = (self.mito
+                                 .filter(c("frame_i") == 
+                                         self.ph3_bud_end[0, "frame_i"])
+                                 )
+
+        daughters = (self.ph3
+                     .filter((c("relationship") == "mother") & (c("cell_cycle_stage") == "S"))
+                     .unique(c("relative_ID"))
+                     .to_series(6) # intended column Cell_ID
+                     )
+        self.parent = parent
+        self.daughters = []
+        for daughter_id in daughters:
+            self.daughters.append(Cell(daughter_id, self, ph3, mito))
 
     def getParentID(self):
-        return self.ph3["relative_ID"]
+        match self.parent:
+            case Cell() as parent:
+                return parent.id
+            case bool():
+                print(f"Cell {self.id} has no ancestor")
+                return -1
 
-    def getID(self):
-        return self.ph3["Cell_ID"]
-
-    def getFrameID(self):
-        return self.ph3["frame_i"]
+    def getBudEndFrame(self):
+        return self.ph3_bud_end[0, "frame_i"]
 
     def getMotherGeneration(self):
-        return self.parent_ph3["generation_num"].item()
+        match self.parent:
+            case Cell() as parent:
+                return (parent.ph3
+                        .filter(c("frame_i") == self.getBudEndFrame())
+                        )[0, "generation_num"]
+            case bool():
+                print(f"Cell {self.id} has no ancestor")
+                return -1
 
     def getMitoToVolume(self):
         fluorescence = "mCardinal_concentration_dataPrepBkgr_from_vol_fl_3D"
         volume = "cell_vol_fl"
-        self.self_ratio = (self.mito[fluorescence] / self.ph3[volume]).item()
+        self.self_ratio = (self.mito_bud_end[0,fluorescence] /
+                           self.ph3_bud_end[0,volume])
+        return self.self_ratio
 
     def getParentMitoToVolume(self):
         fluorescence = "mCardinal_concentration_dataPrepBkgr_from_vol_fl_3D"
         volume = "cell_vol_fl"
-        self.parent_ratio = (self.parent_mito[fluorescence] / self.parent_ph3[volume]).item()
+        match self.parent:
+            case Cell() as parent:
+                parent_mito = (parent.mito
+                               .filter(c("frame_i") == self.getBudEndFrame())
+                               )
+                parent_ph3 = (parent.ph3
+                              .filter(c("frame_i") == self.getBudEndFrame())
+                              )
+            case bool():
+                print(f"Cell {self.id} has no ancestor")
+                self.parent_ratio = -1
+                return
+        self.parent_ratio = parent_mito[0, fluorescence] / parent_ph3[0, volume]
+        return self.parent_ratio
 
     def getSelfToParentRatio(self):
         self.self_to_parent = self.self_ratio / self.parent_ratio
+        return self.self_to_parent
+
+
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from time import time
 from pathlib import Path
 import polars as pl
 from polars import col as c
@@ -6,47 +7,85 @@ from extras.Cell import Cell
 from extras.utils import select_file
 from configparser import ConfigParser
 
-if __name__ == "__main__":
-    config = ConfigParser()
-    config.read("config.ini")
-    ph3csv = Path(select_file("PH3"))
-    mitocsv = Path(select_file("Mito"))
-
-    ph3 = (pl.scan_csv(ph3csv).reverse() 
-           .filter((c("relationship")=="bud") & 
-                   (c("frame_i") != 102))
-           .unique(c("Cell_ID"))
-           .sort(c("Cell_ID"))
-           .collect())
-    total_cells = ph3.height
-
-    dct = defaultdict(list)
-
-    i = 0
-    for row in ph3.iter_rows(named=True):
-        if i%20 == 0:
-            print(f"Processed {i}/{total_cells} cells")
-        cell = Cell(row)
-        if cell.mito.height==0:
-            print(f"No fluorescence data available for ID {cell.getID()}")
-            print(f"Frame of PH3: {cell.getFrameID()}")
-            continue
-        cell.getMitoToVolume()
-        cell.getParentMitoToVolume()
-        cell.getSelfToParentRatio()
-        dct["Bud_ID"].append(int(cell.getID()))
+def pair_ancestor_to_descendant(cell: Cell, dct: defaultdict, counter: int = 0):
+    for daughter in cell.daughters:
+        pair_ancestor_to_descendant(daughter, dct, counter)
+    if cell.parent and cell.valid:
+        dct["Bud_ID"].append(int(cell.id))
         dct["Mother_ID"].append(int(cell.getParentID()))
         dct["Mother_generation"].append(int(cell.getMotherGeneration()))
-        dct["Mito_to_volume_bud"].append(cell.self_ratio)
-        dct["Mito_to_volume_mother"].append(cell.parent_ratio)
-        dct["Bud_to_mother"].append(cell.self_to_parent)
-        i += 1
-    final = pl.from_dict(dct)
-    output_dir = Path(config["PATHS"]["OutputDirectory"]) / "output.xlsx"
-    final.write_excel(output_dir, 
-                      freeze_panes=(1,0),
-                      autofit=True,
-                      autofilter=True,
-                      float_precision=5,
-                      header_format={"bold":True}
+        dct["Mito_to_volume_bud"].append(cell.getMitoToVolume())
+        dct["Mito_to_volume_mother"].append(cell.getParentMitoToVolume())
+        dct["Bud_to_mother"].append(cell.getSelfToParentRatio())
+
+if __name__ == "__main__":
+    start = time()
+    config = ConfigParser()
+    config.read("config.ini")
+    ph3csv = Path("/home/dtzi/Desktop/Position_0/Images/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_acdc_output.csv")
+    mitocsv = Path("/home/dtzi/Desktop/Position_0/Images/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_run_num1_mCardinal_ref_ch_acdc_output_mask_mitoacdc_outputentation.csv")
+    # ph3csv = Path(select_file("PH3"))
+    # mitocsv = Path(select_file("Mito"))
+
+    ph3 = pl.scan_csv(ph3csv)
+
+    cells_ids = ph3.unique("Cell_ID").collect()
+    unknown_history = (ph3
+                      .filter(c("is_history_known") == 0)
+                      .unique(c("Cell_ID"))
+                      .collect()
                       )
+    founding_mothers = (unknown_history
+                      .filter(c("relationship") == "mother")
+                      .to_series(1) # intended column Cell_ID
+                      )
+
+
+    dct = defaultdict(list)
+    for ancestor_id in founding_mothers:
+        anc = Cell(ancestor_id, False, ph3, pl.scan_csv(mitocsv))
+        pair_ancestor_to_descendant(anc, dct)
+
+    df = pl.from_dict(dct).sort(c("Bud_ID"))
+    df.write_excel("output.xlsx")
+    end = time()
+    print(f"Took {round(end-start, 2)} seconds")
+
+    # ph3 = (pl.scan_csv(ph3csv).reverse() 
+    #        .filter((c("relationship")=="bud") &
+    #                (c("frame_i") != 102))
+    #        .unique(c("Cell_ID"))
+    #        .sort(c("Cell_ID"))
+    #        .collect())
+    # total_cells = ph3.height
+
+    # dct = defaultdict(list)
+
+    # i = 0
+    # for row in ph3.iter_rows(named=True):
+    #     if i%20 == 0:
+    #         print(f"Processed {i}/{total_cells} cells")
+    #     cell = Cell(row)
+    #     if cell.mito.height==0:
+    #         print(f"No fluorescence data available for ID {cell.getID()}")
+    #         print(f"Frame of PH3: {cell.getFrameID()}")
+    #         continue
+    #     cell.getMitoToVolume()
+    #     cell.getParentMitoToVolume()
+    #     cell.getSelfToParentRatio()
+    #     dct["Bud_ID"].append(int(cell.getID()))
+    #     dct["Mother_ID"].append(int(cell.getParentID()))
+    #     dct["Mother_generation"].append(int(cell.getMotherGeneration()))
+    #     dct["Mito_to_volume_bud"].append(cell.self_ratio)
+    #     dct["Mito_to_volume_mother"].append(cell.parent_ratio)
+    #     dct["Bud_to_mother"].append(cell.self_to_parent)
+    #     i += 1
+    # final = pl.from_dict(dct)
+    # output_dir = Path(config["PATHS"]["OutputDirectory"]) / "output.xlsx"
+    # final.write_excel(output_dir, 
+    #                   freeze_panes=(1,0),
+    #                   autofit=True,
+    #                   autofilter=True,
+    #                   float_precision=5,
+    #                   header_format={"bold":True}
+    #                   )
