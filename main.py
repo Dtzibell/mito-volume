7b92ba4859078b3f1709c2b1d8b743c25bff37cf
Dtzibell <zaleniakas.tauras@gmail.com>
2026-03-18 14:08:23 +0100

    documentation

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ from polars import col as c
 from modules.Cell import Cell
 from modules.utils import select_file
 from configparser import ConfigParser
-import logging
 import sys
 
-
 def pair_ancestor_to_descendants(cell, dct):
+    """
+    Retrieves data about each cell cycle that relates to the given cell.
+    Recursively goes down the cell's genealogical tree.
+
+    :param cell: a Cell object
+    :param dct: a dictionary where the cell cycles are to be saved
+    """
     for (i, daughter) in enumerate(cell.daughters):
         pair_ancestor_to_descendants(daughter, dct)
         dct["Bud_ID"].append(int(daughter.id))
@@ -24,24 +29,41 @@ def pair_ancestor_to_descendants(cell, dct):
         dct["S_length"].append(cell.getLengthOfS(i))
         dct["Cell_cycle_length"].append(cell.getCellCycleLength(i))
 
+def parse_config(config_file_name: str) -> ConfigParser:
+    """
+    Retrieves the config
+
+    :param config_file_name: name of the file where the configuration is stored.
+    """
+    config = ConfigParser()
+    config.read(config_file_name)
+    print(config.sections())
+    return config
 
 if __name__ == "__main__":
-    config = ConfigParser()
-    config.read("config.ini")
-    # ph3csv = Path(
-    #     "/home/tauras/Desktop/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_acdc_output.csv"
-    # )
-    # mitocsv = Path(
-    #     "/home/tauras/Desktop/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_run_num1_mCardinal_ref_ch_acdc_output_mask_mitoacdc_outputentation.csv"
-    # )
+    config = parse_config("config.ini")
+    # convenience for development
+    # ph3csv = Path("/home/tauras/Desktop/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_acdc_output.csv")
+    # mitocsv = Path("/home/tauras/Desktop/Point0000_ChannelmCardinal_Ph-3_Seq0000_s1_run_num1_mCardinal_ref_ch_acdc_output_mask_mitoacdc_outputentation.csv")
+    
+    # file selection windows. Can modify to automatically select _output and 
+    # _outputentation via pathlib.Path.walk(), but need to be sure that names
+    # are always that. Hence not implemented.
     ph3csv = Path(select_file("PH3"))
     mitocsv = Path(select_file("Mito"))
+
+    # starts run timer, I like it for performance evaluation
     start = time()
 
+    # scan_csv creates polars.LazyFrames, which are not real DataFrames.
+    # They only become real DataFrames once collect is called
     ph3 = pl.scan_csv(ph3csv)
     mito = pl.scan_csv(mitocsv)
-    cum_df = (
-        mito.join(ph3, on=["Cell_ID", "frame_i"], how="left")
+
+    # ph3 dataset is merged on the mito dataset. "left" indicates that mito is 
+    # the reference dataset - only rows that exist in mito will appear in the 
+    # cumulative dataframe (hence the problem with missing first frame for buds)
+    cum_lf = (mito.join(ph3, on=["Cell_ID", "frame_i"], how="left")
         .select(
             [
                 "Cell_ID",
@@ -57,33 +79,57 @@ if __name__ == "__main__":
                 "will_divide",
             ]
         )
-        .filter(c("will_divide") == 1)
-        .set_sorted("frame_i")
-        .collect()
-    )
+              )
+    # It is possible to merge on ph3, but it causes many problems and is super
+    # dirty... Do not recommend.
+
+    # removes the cells that do not divide
+    cum_df = (cum_lf.filter(c("will_divide") == 1)
+              .collect() # makes the DataFrame
+              )
+
+    # splits the dataframe into a dictionary of dataframes for each cell_id
     partitions = cum_df.partition_by("Cell_ID", as_dict=True)
 
+    # finds the experiments imaging rate
     for p in partitions.values():
         if p.height >= 2:
             imaging_rate = p[1, "time_minutes"] - p[0, "time_minutes"]
             break
         else:
-            logging.error("could not determine imaging rate")
+            print("could not determine imaging rate")
             sys.exit(1)
 
+    # founding mothers are cells that do not have progenitors with a known
+    # history. For example, if cell id 1 does not have a known history, all
+    # of its buds throughout the experiment are labeled founding mothers.
     unknown_history_cells = cum_df.filter(c("is_history_known") == 0).unique("Cell_ID")[
         "Cell_ID"
     ]
     founding_mothers = cum_df.filter(
         (c("is_history_known") == 1)
+        # dont know why implode has to be called here, but theres this
+        # https://github.com/pola-rs/polars/pull/22178
         & (c("relative_ID").is_in(unknown_history_cells.implode()))
     )["Cell_ID"].unique()
 
+    # determining founding_mothers allows me to build a tree. E.g.: 
+    #            1 <-- founding mother
+    #           / \
+    #          3  2
+    #         /  / \ ... other cells
+    # working with trees is very intuitive, hence the structure.
+
+    # defaultdict(list) allows list operations to be called on each item regardless
+    # of whether the item is present or not.
     dct = defaultdict(list)
+
     for id in founding_mothers:
+        # refer to modules/Cell.py for documentation
         cell = Cell(id, partitions, imaging_rate)
         pair_ancestor_to_descendants(cell, dct)
 
+    # creates the output csv and terminates the script
     df = pl.from_dict(dct).sort(c("Bud_ID"))
     output_dir = Path(config["PATHS"]["OutputDirectory"])
     output_dir.mkdir(parents=True, exist_ok=True)
--- modules/Cell.py
+++ modules/Cell.py
@@ -1,18 +1,23 @@
-from __future__ import annotations
 import polars as pl
 from polars import col as c
-from polars.exceptions import ColumnNotFoundError
 
-
+# the implementation is not efficient, but it should be fine for now.
 class Cell:
     def __init__(
         self, id: int, partitions: dict, IMAGING_RATE: int
     ):
         self.id = id
+        # partitions are indexed as tuples (id,)
         self.df = partitions.get((id,), pl.DataFrame())
         self.IMAGING_RATE = IMAGING_RATE
+        # further partitions the cell's df by generation number, but it is a list
+        # The 0th index of the list is the first generation of the cell - bud
+        # stage is excluded from here.
         self.generations = self.df.partition_by("generation_num")[1:]
 
+        # builds the tree:
+        # each parent cell nows about its daughter cells, but daughter cells
+        # do not know about their parents.
         self.daughters = []
         for gen in self.generations:
             daughter_id = self.getDaughterID(gen)
@@ -22,129 +27,60 @@ class Cell:
         return gen[-1, "relative_ID"]
 
     def getMitoToVolumeBud(self):
+        """
+        Retrieves the ratio of mitochondrial fluorescent signal and the cell's
+        volume at the end of the cell's budding phase
+        """
         fluorescence = "mCardinal_concentration_dataPrepBkgr_from_vol_fl_3D"
         # Apparently, mito datasets have a cell_vol_fl as well. It probably
         # refers to the volume of the mitochondria. After right join of ph3
         # to mito, the ph3 cell_vol_fl is renamed to cell_vol_fl_right
         volume = "cell_vol_fl_right"
         bud_end = self.df.filter(c("generation_num") == 0)[-1]
+        #[0, x] retrieves the 0th row, xth column
         return bud_end[0, fluorescence] / bud_end[0, volume]
 
     def getMitoToVolumeGen(self, gen):
+        """
+        Retrieves the ratio of mitochondrial fluorescent signal and the cell's
+        volume at the end of the cell's generation
+        :param gen:
+        """
         fluorescence = "mCardinal_concentration_dataPrepBkgr_from_vol_fl_3D"
         # Apparently, mito datasets have a cell_vol_fl as well. It probably
         # refers to the volume of the mitochondria. After right join of ph3
         # to mito, the ph3 cell_vol_fl is renamed to cell_vol_fl_right
         volume = "cell_vol_fl_right"
-        gen_end = self.df.filter(c("generation_num") == gen + 1)[-1]
+        gen_end = self.generations[gen][-1] # -1 gets the last row of the generation
         return gen_end[0, fluorescence] / gen_end[0, volume]
 
     def getSizeAtEndOfBud(self):
+        """
+        Retrieves the size of the cell at the end of its budding stage
+        """
         volume = "cell_vol_fl_right"
         bud_end = self.df.filter(c("generation_num") == 0)[-1]
         return bud_end[0, volume]
     
     def getLengthOfG1(self, gen):
+        """
+        Retrieves the length of G1
+        """
         return (self.generations[gen]
                 .filter(c("cell_cycle_stage") == "G1")
                 .height * self.IMAGING_RATE)
 
     def getLengthOfS(self, gen):
+        """
+        Retrieves the length of S
+        """
         return (self.generations[gen]
                 .filter(c("cell_cycle_stage") == "S")
                 .height * self.IMAGING_RATE
                 )
 
     def getCellCycleLength(self, gen):
+        """
+        Retrieves the length of the cell cycle.
+        """
         return self.getLengthOfG1(gen) + self.getLengthOfS(gen)
-
-    #     self.first_cell_cycle = self.df.filter(c("generation_num").is_in([0, 1]))
-    #     self.budding = self.first_cell_cycle.filter(c("cell_cycle_stage") == "S")
-    #
-    #     # sets valid flag for cells that have finished a full S phase.
-    #     if self.budding.height < self.df.height:
-    #         self.valid = True
-    #     else:
-    #         self.valid = False
-    #     if self.valid and parent:
-    #         self.bud_end = self.budding.filter(c("relationship") == "bud")[-1]
-    #
-    #     daughters = (
-    #         self.df.lazy()
-    #         .filter((c("relationship") == "mother") & (c("cell_cycle_stage") == "S"))
-    #         .unique(c("relative_ID"))
-    #         .collect()
-    #         .get_column("relative_ID")
-    #     )
-    #     self.daughters = []
-    #     for daughter_id in daughters:
-    #         try:
-    #             self.daughters.append(Cell(daughter_id, self, partitions, IMAGING_RATE))
-    #         except ColumnNotFoundError:
-    #             print(
-    #                 f"{int(daughter_id)} is not present within the mitochondria dataset"
-    #             )
-    #             continue
-    #
-    # def getParentID(self):
-    #     match self.parent:
-    #         case Cell() as parent:
-    #             return parent.id
-    #         case bool():
-    #             print(f"Cell {self.id} has no ancestor")
-    #             return -1
-    #
-    # def getBudEndFrame(self):
-    #     return self.bud_end[0, "frame_i"]
-    #
-    # def getMotherGeneration(self):
-    #     match self.parent:
-    #         case Cell() as parent:
-    #             return (parent.df.filter(c("frame_i") == self.getBudEndFrame()))[
-    #                 0, "generation_num"
-    #             ]
-    #         case bool():
-    #             print(f"Cell {self.id} has no ancestor")
-    #             return -1
-    #
-    # def getMitoToVolume(self):
-    #     fluorescence = "mCardinal_concentration_dataPrepBkgr_from_vol_fl_3D"
-    #     # Apparently, mito datasets have a cell_vol_fl as well. It probably
-    #     # refers to the volume of the mitochondria. After right join of ph3
-    #     # to mito, the ph3 cell_vol_fl is renamed to cell_vol_fl_right
-    #     volume = "cell_vol_fl_right"
-    #
-    #     self.self_ratio = self.bud_end[0, fluorescence] / self.bud_end[0, volume]
-    #     return self.self_ratio
-    #
-    # def getParentMitoToVolume(self):
-    #     fluorescence = "mCardinal_concentration_dataPrepBkgr_from_vol_fl_3D"
-    #     # Apparently, mito datasets have a cell_vol_fl as well. It probably
-    #     # refers to the volume of the mitochondria. After right join of ph3
-    #     # to mito, the ph3 cell_vol_fl is renamed to cell_vol_fl_right
-    #     volume = "cell_vol_fl_right"
-    #     match self.parent:
-    #         case Cell() as parent:
-    #             parent_frame = parent.df.filter(c("frame_i") == self.getBudEndFrame())
-    #         case bool():
-    #             print(f"Cell {self.id} has no ancestor")
-    #             self.parent_ratio = -1
-    #             return
-    #     self.parent_ratio = parent_frame[0, fluorescence] / parent_frame[0, volume]
-    #     return self.parent_ratio
-    #
-    # def getSelfToParentRatio(self):
-    #     self.self_to_parent = self.self_ratio / self.parent_ratio
-    #     return self.self_to_parent
-    #
-    # def getLengthOfS(self):
-    #     return self.budding.height * self.IMAGING_RATE
-    #
-    # def getLengthOfG1(self):
-    #     return (self.first_cell_cycle.height - self.budding.height) * self.IMAGING_RATE
-    #
-    # def getCellCycleLength(self):
-    #     return self.first_cell_cycle.height * self.IMAGING_RATE
-    #
-    # def getSizeAtEndOfS(self):
-    #     return self.budding[-1, "cell_vol_fl_right"]
